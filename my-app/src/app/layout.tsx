2b4fb919a177ea431e9e4b0a4ecaae90225f0a27
romanohu <shi301649@gmail.com>
2025-05-22 10:22:46 +0900

    仮完成

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,13 +1,17 @@
 export const metadata = {
   title: 'Romanohu Blog',
   description: 'Romanohu Blog',
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 // children は各ページ（page.tsx）の中身
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>
+      <body className= "mx-auto w-1/2 bg-gray-200">
         {/* 共通のヘッダー */}
         <header>
           <h1>RomaLog</h1>
