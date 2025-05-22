0a5c8f2154839972787df46a188edbc7f0104f37
romanohu <shi301649@gmail.com>
2025-05-22 20:01:15 +0900

    Layout

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="w-full max-w-4xl bg-white p-8 rounded shadow">適当なことを投稿するブログ</h1>
-      <p className="w-full max-w-4xl bg-white p-8 rounded shadow">少し装飾がついた</p>
       <p className="w-full max-w-4xl bg-white p-8 rounded shadow">
         <Link href={`/posts`} className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">
           投稿一覧へ
--- my-app/src/app/posts/page.tsx
+++ my-app/src/app/posts/page.tsx
@@ -5,19 +5,25 @@ export default function PostListPage() {
   const posts = getAllPosts();
 
   return (
-    <div>
-      <h1 className="w-full max-w-4xl bg-white p-8 rounded shadow">投稿一覧</h1>
-
-      {posts.map((post) => (
-        <div key={post.slug} className="w-full max-w-4xl bg-white p-8 rounded shadow">
-          <h2 >
-            <Link href={`/posts/${post.slug}`}>
-              {post.title}
-            </Link>
-          </h2>
-          <p>{new Date(post.date).toLocaleDateString("ja-JP")}</p>
-        </div>
-      ))}
+    <div className="px-4 sm:px-8">
+      <div className="grid gap-0.5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {posts.map((post) => (
+          <div key={post.slug} className="bg-white p-6 rounded shadow">
+            <h2 className="text-xl font-semibold mb-2">
+              <Link
+                href={`/posts/${post.slug}`}
+                className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors"
+              >
+                {post.title}
+              </Link>
+            </h2>
+            <p className="text-sm text-gray-600">
+              {new Date(post.date).toLocaleDateString("ja-JP")}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 }
