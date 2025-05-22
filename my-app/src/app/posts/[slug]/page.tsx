9f16a508271b50bd49a443d4f2fcca22bc9bab6b
romanohu <shi301649@gmail.com>
2025-05-22 12:15:42 +0900

    make_1

--- my-app/src/app/posts/[slug]/page.tsx
+++ my-app/src/app/posts/[slug]/page.tsx
@@ -18,21 +18,20 @@ export default async function Page({ params }: any) {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-6 space-y-4">
-      <h1 className="text-2xl font-bold">{post.title}</h1>
+    <div>
+      <h1>{post.title}</h1>
 
-      <p className="text-sm text-gray-500">
+      <p>
         投稿日: {new Date(post.date).toLocaleDateString("ja-JP")}
       </p>
 
-      <div className="flex gap-2 text-sm text-blue-700">
+      <div>
         {post.tags?.map((tag: string) => (
           <span key={tag}>#{tag}</span>
         ))}
       </div>
 
       <div
-        className="prose mt-6"
         dangerouslySetInnerHTML={{ __html: post.contentHtml }}
       />
     </div>
