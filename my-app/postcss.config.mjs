58cf171715da1fd559bf68de89aef1146244e34f
romanohu <shi301649@gmail.com>
2025-05-22 13:04:00 +0900

    CSS

--- my-app/postcss.config.mjs
+++ my-app/postcss.config.mjs
@@ -1,24 +1,17 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-  tailwindcss: {
-    config: "./tailwind.config.js",
-  },
-  autoprefixer: {
-    grid: true,
-    flexbox: true,
-  },
-  "postcss-nested": {},
-  "postcss-custom-properties": {
-    preserve: false,
-  },
-  "postcss-preset-env": {
-    stage: 1,
-    features: {
-      "custom-properties": true,
-      "nesting-rules": true,
+  plugins: {
+    tailwindcss: { config: './tailwind.config.js' },
+    autoprefixer: { grid: true, flexbox: true },
+    'postcss-nested': {},
+    'postcss-custom-properties': { preserve: false },
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'custom-properties': true,
+        'nesting-rules': true,
+      },
     },
   },
-  
 };
 
 export default config;
