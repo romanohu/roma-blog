a411b9d5ea7a09e0131e7e702dfcba25b8778585
romanohu <shi301649@gmail.com>
2025-05-22 12:59:07 +0900

    CSS

--- my-app/postcss.config.mjs
+++ my-app/postcss.config.mjs
@@ -1,5 +1,24 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  tailwindcss: {
+    config: "./tailwind.config.js",
+  },
+  autoprefixer: {
+    grid: true,
+    flexbox: true,
+  },
+  "postcss-nested": {},
+  "postcss-custom-properties": {
+    preserve: false,
+  },
+  "postcss-preset-env": {
+    stage: 1,
+    features: {
+      "custom-properties": true,
+      "nesting-rules": true,
+    },
+  },
+  
 };
 
 export default config;
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </header>
 
         {/* ページの中身（個別ページがここに差し込まれる） */}
-        <main className= "grid place-items-center h-screen">{children}</main>
+        <main className= "bg-white/10 backdrop-blur-md rounded-xl p-4 shadow-lg">{children}</main>
 
         {/* 共通のフッター */}
         <footer className= "grid place-items-center h-screen">
