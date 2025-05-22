801663ae7c71a4538792094c35d7851b2674271b
romanohu <shi301649@gmail.com>
2025-05-22 12:05:56 +0900

    make_1

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h1>ようこそ！</h1>
+      <h1>適当なことを投稿するブログ</h1>
+      <p>現状，装飾なし……</p>
       <Link href={`/posts`}>
         投稿一覧へ
       </Link>
