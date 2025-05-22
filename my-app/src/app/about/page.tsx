10ef0597ac66987b26be791d313eba90caffaa66
romanohu <shi301649@gmail.com>
2025-05-22 12:40:03 +0900

    about me

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>ロマノフです．</p>
+      <p>私はロシア人です．</p>
+      <p>mdで記事を追加できるのにJSXでこのページを作っている意味が分かりません</p>
+    </div>
+  );
+}
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,9 +6,17 @@ export default function Home() {
     <main>
       <h1>適当なことを投稿するブログ</h1>
       <p>現状，装飾なし……</p>
-      <Link href={`/posts`}>
-        投稿一覧へ
-      </Link>
+      <p>
+        <Link href={`/posts`}>
+          投稿一覧へ
+        </Link>
+      </p>
+      <p>
+        <Link href={`/about`}>
+          about me
+        </Link>
+      </p>
+
     </main>
   );
 }
