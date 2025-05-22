d347155a744ae8a9037a5545584d26e9db6c637c
romanohu <shi301649@gmail.com>
2025-05-22 19:22:14 +0900

    about

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
       <h1>About Me</h1>
       <p>ロマノフです．</p>
       <p>私はロシア人です．</p>
-      <p>mdで記事を追加できるのにJSXでこのページを作っている意味が分かりません</p>
+      <p>mdで記事を追加できるのにJSXでこのページを書いている……，謎だな</p>
     </div>
   );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
       <h1 className="w-full max-w-4xl bg-white p-8 rounded shadow">適当なことを投稿するブログ</h1>
       <p className="w-full max-w-4xl bg-white p-8 rounded shadow">少し装飾がついた</p>
       <p className="w-full max-w-4xl bg-white p-8 rounded shadow">
-        <Link href={`/posts`}　className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">
+        <Link href={`/posts`} className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">
           投稿一覧へ
         </Link>
       </p>
       <p className="w-full max-w-4xl bg-white p-8 rounded shadow">
-        <Link href={`/about`}　className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">
+        <Link href={`/about`} className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">
           about me
         </Link>
       </p>
