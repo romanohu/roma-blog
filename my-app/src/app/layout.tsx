de7e8eae6aa15d698e93e86b4933750c4e4b74d7
romanohu <shi301649@gmail.com>
2025-05-22 15:40:57 +0900

    CSS

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -13,10 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>
+      <body className="grid place-items-center min-h-screen bg-gray-100">
   
         <header>
-          <h1>RomaLog</h1>
+          <div className="grid place-items-center  bg-gray-100">
+            <div className="w-full max-w-4xl bg-white p-8 rounded shadow">
+              <h1 className="text-4xl font-semibold text-center -ml-4 font-serif tracking-wide text-gray-800">
+                RomaLog
+             </h1> 
+            </div>
+          </div>
         </header>
 
         
