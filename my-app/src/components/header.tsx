133ab6cf851c547d7f07b32c2cbd6eff1198316c
romanohu <shi301649@gmail.com>
2025-05-26 21:42:58 +0900

    CSSの修正

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -12,8 +12,8 @@ type POSTS = {
 export default function Header({ posts }: { posts: POSTS[] }) {
     return(
         <header>
-            <div className="gnavi__wrapmx-auto px-2 sm:px-4 ">
-                <ul className="gnavi__lists">
+            <div className="gnavi__wrap">
+                <ul className="gnavi__lists gnavi__wrapmx-auto">
                     <li className="gnavi__list"><Link href={'/'}className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors"><div >Roma.Log(/・・)/</div></Link></li>
                     <li className="gnavi__list"><Link href={`/posts`} className="text-indigo-600 font-bold underline hover:text-indigo-800 transition-colors">記事一覧</Link>
                         <ul className="dropdown__lists">
