a57062a1b904bf6a1669f8395db9e4bf2d4fe7b4
romanohu <shi301649@gmail.com>
2025-06-19 02:45:52 +0900

    CSS振り直し

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -1,14 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="w-full bg-gray-100 py-4 shadow">
       <div className="container mx-auto flex justify-between items-center px-4">
         <h1 className="text-xl font-bold">
-          <a href="/">Roma.Log(/・・)/</a>
+          <Link href="/">Roma.Log(/・・)/</Link>
         </h1>
         <nav className="flex gap-4">
-          <a className="hover:underline" href="/about">About</a>
-          <a className="hover:underline" href="/posts">Posts</a>
-          <a className="hover:underline" href="/booklog">Booklog</a>
+          <Link className="hover:underline" href="/">ホーム</Link>
+          <Link className="hover:underline" href="/about">About</Link>
+          <Link className="hover:underline" href="/posts">Posts</Link>
+          <Link className="hover:underline" href="/booklog">Booklog</Link>
         </nav>
       </div>
     </header>
